0a3755bb4abd71ed8ef250b7dd5605247a21676d
Yonela <yonela@muluma.co.za>
2024-06-25 11:44:31 +0200

    git push

--- MyVariables.js
+++ MyVariables.js
@@ -7,18 +7,18 @@ if (userName) {
 
 // Define and log the variables language and score
 const language = "JavaScript";
-const score = 10;
+const score = 20;
 console.log("Language: " + language);
 console.log("Score: " + score);
 
 // Define variables length and width, calculate area, and log the result
-const length = 5;
+const length = 80;
 const width = 10;
 const area = length * width;
 console.log("The area of the rectangle is " + area);
 
 // Define variables num1 and num2, calculate remainder, and log the result
-const num1 = 20;
-const num2 = 6;
+const num1 = 10;
+const num2 = 4;
 const remainder = num1 % num2;
 console.log("The remainder when " + num1 + " is divided by " + num2 + " is " + remainder);
